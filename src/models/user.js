5c07a67a4c97c494e6707869587a30e0591a58ac
Drew Nauman <din3@cornell.edu>
2016-06-30 16:58:55 -0600

    finished server routes and user model

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
+
+var userSchema = new Schema ({
+   name: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    },
+    age: Number,
+    height: Number,
+    weight: Number,
+    gender: String,
+    goal: String
+});
--- src/routes/mealRoutes.js
+++ src/routes/mealRoutes.js
@@ -1,3 +1,41 @@
 var express = require("express");
 var mealRouter = express.Router();
-var Meal = require(../models/meal)
+var Meal = require("../models/meal");
+
+mealRouter.route("/")
+    .get(function (req, res) {
+        Loan.find({user: req.user._id}, function (err, meals) {
+            if (err) res.status(500).send(err);
+            else res.send(meals);
+        });
+    })
+    .post(function (req, res) {
+        var newLoan = new Loan(req.body);
+        newLoan.user = req.user._id;
+        newLoan.save(function(err, user) {
+            if(err) res.status(500).send(err);
+            else res.status(201).send(user);
+        });
+});
+
+mealRouter.route("/:mealId")
+    .get(function (req, res) {
+        Loan.findOne({_id: req.params.mealId, user: req.user._id}, function (err, meal) {
+            if (err) res.status(500).send(err);
+            else res.send(meal);
+        });
+    })
+    .put(function (req, res) {
+        Loan.findOneAndUpdate({_id: req.params.mealId, user: req.user._id}, req.body, {new:true}, function(err, meal) {
+            if (err) res.send(500).send(err);
+            else res.send(meal);
+        });
+    })
+    .delete(function (req, res) {
+        Loan.findOneAndRemove({_id: req.params.mealId, user: req.user._id}, function(err, meal) {
+            if (err) res.status(500).send(err);
+            else res.send(meal);
+        });
+    });
+
+module.exports = mealRouter;
