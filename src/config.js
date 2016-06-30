c3cba935fe396f1cecae1df7b0318bd934f9d720
Drew Nauman <din3@cornell.edu>
2016-06-30 12:17:27 -0600

    completed server.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: "mongodb://localhost/calorie",
+    secret: "valar morghulis"
+}
---
+++ src/server.js
@@ -0,0 +1,29 @@
+var express = require("espress");
+var app = express();
+var bcrypt = require("bcrypt");
+var bodyParser = require("body-parser");
+var config = require("./config.js");
+var cors = require("cors");
+var expressJwt = require("express-jwt");
+var mongoose = require("mongoose");
+var morgan = require("morgan");
+var path = require("path");
+var port = process.env.PORT || 8000;
+
+mongoose.connect(config.database, function() {
+    console.log("Connection to MongoDB successful.")
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use("/api", expressJwt({secret: config.secret}));
+
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api/meals", require("./routes/mealRoutes"));
+
+app.listen(port, function() {
+    console.log("App is lisetening on Port" + port + ".")
+});
