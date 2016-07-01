e5976b57fc8bc6dc0a027bf0e3494403af641e95
aburkex24 <aburkex24@gmail.com>
2016-07-01 09:08:45 -0600

    pushing add and delete meal functions

--- public/components/services/mealService.js
+++ public/components/services/mealService.js
@@ -1,5 +1,26 @@
 var app = angular.module("CalorieApp");
 
 app.service("MealService", ["$http", function($http) {
+    var self = this; 
+    this.mealList = [];
+    var baseUrl = "/api/meals"
+    
+    this.getMeals = function() {
+        return $http.get(baseUrl).then(function(response) {
+            self.mealList = response.data; 
+        });
+    } 
+    
+    this.addMeal = function(meal) {
+        return $http.post(baseUrl, meal  ).then(function(response) {
+            self.mealList.push(response.data); 
+        });
+    }
+    
+    this.deleteMeal = function(index, meal) {
+        return $http.delete(baseUrl + meal._id).then(function(response) {
+            self.mealList.splice(index, 1); 
+        });
+    }
     
 }]);
